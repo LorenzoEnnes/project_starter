5b9381a5db271a4403fccd53fef6a482df10452e
Lorenzo Ennes <lorenzocennes@gmail.com>
2025-02-12 23:48:44 -0500

    sql new script + 1 topic idea on project_1_topic

---
+++ my_first_sql.sql
@@ -0,0 +1,7 @@
+select * from client_1.departament.customer_table;
+--                 |        |           |
+--            database      |           |
+--                       schema         |
+--                                   tables/views
+
+select * from customer_name;
--- project_1_topics.py
+++ project_1_topics.py
@@ -1,2 +1,4 @@
 ## Here in this script we will write the ideas of our project
 
+## Maybe an API idea for Spotify.
+
